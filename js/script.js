432fec69c04d4aaa78bed844d56b466d908d96c4
ArtemUholkov <artem.ugolkov@ukr.net>
2024-09-04 15:11:41 +0300

    added all components, sliders works

--- js/script.js
+++ js/script.js
@@ -35,3 +35,14 @@ function burgerHandler(e) {
   }
   burger.classList.toggle('active');
 }
+var className = 'inverted';
+var scrollTrigger = 50;
+
+window.onscroll = function () {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByClassName('header_wrapper')[0].classList.add(className);
+  } else {
+    document.getElementsByClassName('header_wrapper')[0].classList.remove(className);
+  }
+};
